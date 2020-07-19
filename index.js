86d9b4f1c91937c04d9893be79cbcb7e6349cd8d
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-19 06:11:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -180,12 +180,12 @@ function moveDodger(e) {
   if (e.which === LEFT_ARROW) {
     moveDodgerLeft()
     e.stopPropagation()
-    e.stopDefault()
+    e.preventDefault()
   }
   else if(e.which === RIGHT_ARROW){
     moveDodgerRight()
     e.stopPropagation()
-    e.stopDefault()
+    e.preventDefault()
   }
   
 }
