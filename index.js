6f2fd42a485ef14854e80b79ba78ba34e8c0bbce
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-19 05:32:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -171,12 +171,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
   document.addEventListener('keydown', function(e) {
-    if (e.which === LEFT_ARROW) {
-      moveDodgerLeft()
-    }
-    else if(e.which === RIGHT_ARROW){
-      moveDodgerRight()
-    }
+    
   })
 }
 
